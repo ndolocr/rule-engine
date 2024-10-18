ad00ee06622d9272eb61e71b20ba5bc1e77c0e6e
ndolocr <crndolo@gmail.com>
2024-10-18 12:35:58 +0300

    Create a url to get all rules from the database.

--- KnowledgeBase/urls.py
+++ KnowledgeBase/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from KnowledgeBase import views
 
 urlpatterns = [
-    path('get/rules/by/namespace/<str:namespace>', views.getRuleByNamespace, name='get_by_namespace'),
+    path('get/all/rules', views.getAllRules, name='get_all_rules'),
+    path('get/rules/by/namespace/<str:namespace>', views.getRuleByNamespace, name='get_rules_by_namespace'),
 ]
