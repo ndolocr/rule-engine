71dcc71d4be360227b696f5d4b7d15fb6e766dd9
ndolocr <crndolo@gmail.com>
2024-11-11 13:02:04 +0300

    Create url for v2 function

--- restApi/urls.py
+++ restApi/urls.py
@@ -4,5 +4,6 @@ from restApi import views
 
 urlpatterns = [
     path('get', views.get_transaction, name='get_transaction'),
-    path('process', views.process_transaction, name='process_transaction')
+    path('process', views.process_transaction, name='process_transaction'),
+    path('process_v2', views.process_V2_transaction, name='process_V2_transaction'),
 ]
